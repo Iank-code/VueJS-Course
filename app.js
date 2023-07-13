c6f63e47961028dfd06b396e6f7e2b77dc9aefe2
ianK-code <kian99564@gmail.com>
2023-07-13 23:05:17 +0300

    v-model

--- app.js
+++ app.js
@@ -3,11 +3,20 @@ const vm = Vue.createApp({
     return {
       firstname: "John",
       lastname: "Doe",
+      url: "https://google.com",
+      raw_url: '<a href="http://google.com" target="_blank">Google</a>',
+      age: 20,
     };
   },
   methods: {
     fullName() {
       return `${this.firstname} ${this.lastname.toUpperCase()}`;
+    },
+    increment() {
+      this.age++;
+    },
+    updateLastName(event) {
+      this.lastname = event.target.value;
     },
   },
 }).mount("#app");
